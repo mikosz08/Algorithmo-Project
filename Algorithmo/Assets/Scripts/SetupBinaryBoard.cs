d398f788b0c1465e3ad3a6c11972d82e3b770be6
mikosz08 <46966211+mikosz08@users.noreply.github.com>
2023-03-13 21:31:16 +0100

    Adds BinarySearchBoard

--- Algorithmo/Assets/Scripts/SetupBinaryBoard.cs
+++ Algorithmo/Assets/Scripts/SetupBinaryBoard.cs
@@ -1,31 +1,40 @@
+using System.Data;
 using System.Collections;
 using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 
 public class SetupBinaryBoard : MonoBehaviour
 {
-
     private BinarySearch binarySearch;
 
-    [SerializeField] private GameObject gameBallPrefab = null;
-    [SerializeField] private Vector2 boardSize = Vector2.zero;
-    [SerializeField] private Vector2 instantiateOffset = Vector2.right;
+    [SerializeField] private GameObject prefab = null;
     [SerializeField] private Transform instantiateParent = null;
-
+    [SerializeField] private Vector3 prefabSize = new Vector3(1, 1, 1);
+    [SerializeField] private Vector2 instantiateOffset = Vector2.right;
+    [SerializeField] private Vector2 boardSize = Vector2.zero;
     [SerializeField] private bool shouldResetBoard = false;
-    [SerializeField] private bool doneSetup = false;
+    [SerializeField] private bool setupDone = false;
 
-    private List<GameObject> prefabList = null;
+    private Vector3 boardPosition = Vector3.zero;
+
+    private List<GameObject> prefabsList = null;
+
+    private void Awake()
+    {
+        binarySearch = GetComponent<BinarySearch>();
+    }
 
     private void Start()
     {
         SetUpBoard();
-        binarySearch = GetComponent<BinarySearch>();
+        StartCoroutine(ActivatePrefabs());
+        binarySearch.SetData(prefabsList);
     }
 
     private void Update()
     {
-        if (doneSetup && shouldResetBoard)
+        if (setupDone && shouldResetBoard)
         {
             Reset();
         }
@@ -33,48 +42,53 @@ public class SetupBinaryBoard : MonoBehaviour
 
     private void Reset()
     {
-        foreach (var p in prefabList)
+        foreach (var p in prefabsList)
         {
             Destroy(p.gameObject);
         }
+        prefabsList = null;
         shouldResetBoard = false;
-        doneSetup = false;
-        SetUpBoard();
+        setupDone = false;
+        Start();
     }
 
     private void SetUpBoard()
     {
-        prefabList = new List<GameObject>();
+        prefabsList = new List<GameObject>();
 
         var position = transform.position;
         var startY = position.y;
+
         for (var x = 0; x < boardSize.x; ++x)
         {
+            // Instantiate given prefabs, on given position and inside given parent:
             for (var y = 0; y < boardSize.y; ++y)
             {
-                var prefab = Instantiate(gameBallPrefab, position, transform.rotation, instantiateParent);
-                prefabList.Add(prefab);
+                var _prefab = Instantiate(prefab, position, transform.rotation, instantiateParent);
+                prefabsList.Add(_prefab);
                 position.y += instantiateOffset.y;
             }
+            // Update next prefab's spawn position:
             position.x += instantiateOffset.x;
             position.y = startY;
         }
-        StartCoroutine(ActivatePrefabs());
+        var _index = prefabsList.Count - 1;
+        var _boardXPos = prefabsList[_index].transform.position.x;
+        transform.position = new Vector3(_boardXPos / -2, 0, transform.position.z);
     }
 
     private IEnumerator ActivatePrefabs()
     {
-        var tmpList = new List<GameObject>(prefabList);
-        while (prefabList.Count > 0)
+        var _tmpPrefabsList = new List<GameObject>(prefabsList);
+        while (_tmpPrefabsList.Count > 0)
         {
-            var randomIndex = Random.Range(0, prefabList.Count);
-            var prefabToActivate = prefabList[randomIndex];
+            var _randomIndex = Random.Range(0, _tmpPrefabsList.Count);
+            var _prefabToActivate = _tmpPrefabsList[_randomIndex];
 
-            prefabToActivate.SetActive(true);
-            prefabList.Remove(prefabToActivate);
-            yield return new WaitForSeconds(0.01f);
+            _prefabToActivate.SetActive(true);
+            _tmpPrefabsList.Remove(_prefabToActivate);
+            yield return new WaitForSeconds(0.001f);
         }
-        prefabList = tmpList;
-        doneSetup = true;
+        setupDone = true;
     }
 }
